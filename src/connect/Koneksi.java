7cf17da02d1814b820005df119afc73803d7e343
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-04-28 17:50:38 +0700

    commit heula

---
+++ src/connect/Koneksi.java
@@ -0,0 +1,29 @@
+package connect;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+/**
+ *
+ * @author Anditeguh
+ */
+public class Koneksi {
+    
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:8889/uts_visual","root","root");
+            return con;
+        } 
+        catch(Exception e){
+            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+            return null;
+        }
+    }
+
+    public static PreparedStatement getCon(String sql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
